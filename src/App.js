4f4c0488af55af583ebc2c6602c8f19da945eb8e
Tuhirirwe-Maria <tuhirirwemaria@gmail.com>
2021-12-15 13:05:10 +0300

    FEAT: add cards to jobs page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Routes, Route} from "react-router-dom";
-import Home from "./pages/Home/Home";
 import SignIn from "./pages/SignIn/SignIn";
 import SignUp from "./pages/SignUp/SignUp";
-import Jobs from "./pages/Jobs/Jobs";
 import Error from "./pages/404/Error";
 import Nav from "./components/Nav/Navbar";
 
@@ -15,16 +13,16 @@ import Profile from "./pages/Profile/Profile";
 import Footer from "./components/Footer/Footer";
 import './App.css';
 import Jobid from "./pages/Jobid/Jobid";
+import Jobs from "./pages/NewJobs/Jobs";
 
 function App() {
   return (
     <div className="App">
       <Nav/>
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/jobs" element={<Jobs/>} />
         <Route path="/signin" element={<SignIn/>} />
         <Route path="/signup" element={<SignUp/>} />
-        <Route path="/jobs" element={<Jobs/>} />
         <Route path="/viewprofile/:id" element={<ViewProfile/>} />
         <Route path="*" element={ <Error/>} />
         <Route path="/createprofile" element={<CreateProfile/>} />
---
+++ src/pages/NewJobs/Jobs.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import "./Jobs.css"
+const Jobs = () => {
+    return (
+        <>
+            <div className='Container'>
+                <div className='search'>
+              <input type = "text" placeholder = "Company"/>
+              </div>
+          
+            <div className  = "jobsContainer">
+                <div className='jobCard'>
+                    <h4 className='jobTitle'>
+                    Lorem ipsumt amet consectetur
+                    </h4>
+                    <h5 className='jobArea'>
+                     Amet consectetur
+                    </h5>
+                     <p>eveniet aliquid</p>
+                     <p className='jobDescription'>
+                     ipsum debitis quas aliquid. Reprehenderit,
+                    quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
+                    sapiente officiis modi at sunt excepturi 
+                     </p>
+                     <p className='readmo'>Read more </p>
+                     <p className='deadline'>Deadline: 2/2/2022</p>
+                </div>
+                <div className='jobCard'>
+                    <h4 className='jobTitle'>
+                    Lorem ipsumt amet consectetur
+                    </h4>
+                    <h5 className='jobArea'>
+                     Amet consectetur
+                    </h5>
+                     <p>eveniet aliquid</p>
+                     <p className='jobDescription'>
+                     ipsum debitis quas aliquid. Reprehenderit,
+                    quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
+                    sapiente officiis modi at sunt excepturi 
+                     </p>
+                     <p className='readmo'>Read more </p>
+                     <p className='deadline'>Deadline: 2/2/2022</p>
+                </div>
+                <div className='jobCard'>
+                    <h4 className='jobTitle'>
+                    Lorem ipsumt amet consectetur
+                    </h4>
+                    <h5 className='jobArea'>
+                     Amet consectetur
+                    </h5>
+                     <p>eveniet aliquid</p>
+                     <p className='jobDescription'>
+                     ipsum debitis quas aliquid. Reprehenderit,
+                    quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
+                    sapiente officiis modi at sunt excepturi 
+                     </p>
+                     <p className='readmo'>Read more </p>
+                     <p className='deadline'>Deadline: 2/2/2022</p>
+                </div>
+            </div>
+            </div>
+        </>
+    )
+}
+
+export default Jobs
